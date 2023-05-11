14992a1416bd940c570ba6be9f578741e3fcbb3a
joshyan1 <76125168+joshyan1@users.noreply.github.com>
2023-05-11 13:32:56 -0700

    Updated!

--- time table project/main.py
+++ time table project/main.py
@@ -6,19 +6,54 @@ class Person:
         self.id = id
         self.courses = courses
         self.alts = alts
+        self.timetable = Timetable()
 
     def __str__(self):
         return f'\n>>>>>>>>>>>>>\nID is {self.id} \n {self.courses} \n {self.alts} \n>>>>>>>>>>>>>\n'
     
     def __repr__(self):
         return self.__str__()
+    
+    def getID(self):
+        return self.id
+    
+    def getCourses(self):
+        return self.courses
+    
+    def getAlts(self):
+        return self.alts
+#Course Class   
+class Course:
+    def __init__(self, classID, name, baseTermsPerYear , coveredTermsPerYear, maxEnrollment, PPC, sections):
+        self.classID = classID
+        self.name = name
+        self.baseTermsPerYear = baseTermsPerYear
+        self.coveredTermsPerYear = coveredTermsPerYear
+        self.maxEnrollment = maxEnrollment
+        self.PPC = PPC
+        self.sections = sections
+    
+    def __str__(self):
+        return f'\n>>>>>>>>>>>>>\n{self.classID}: {self.name} \n{self.baseTermsPerYear}, {self.coveredTermsPerYear} \n{self.maxEnrollment}, {self.PPC}, {self.sections} \n>>>>>>>>>>>>>\n'
+    
+    def __repr__(self):
+        return self.__str__()
+
+class Timetable:
+    def __init__(self):
+        self.assignedCourses = []
+        
+    def addCourse(self, course):
+        self.assignedCourses.append(course)
 
 #Main   
 people = []
+classes = []
 tempCourses = []
 altCourses = []
 id = 0
 first = True
+
     
 with open('data/requests.csv') as csv_file:
     csv_reader = csv.reader(csv_file)
@@ -43,4 +78,12 @@ with open('data/requests.csv') as csv_file:
             altCourses.append(row[0])
         else: tempCourses.append(row[0])
 
-print(people)
+with open('data/Course Information.csv') as file:
+    csv_reader = csv.reader(file)
+    for row in csv_reader:
+        print(row)
+        if (row[0] != "" and row [1] != ""):
+            newCourse = Course(row[0], row[2], row[7], row[8], row[9], row[10], row[14])
+            classes.append(newCourse)
+        
+print(classes)
